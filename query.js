0d30acb2a460001f60a758de92b5243c76849f69
infectious_07 <kotharidivit07@gmail.com>
2022-02-20 08:26:33 +0000

    ReWrite

--- query.js
+++ query.js
@@ -1,9 +1,8 @@
-function submit() {
+function submit(val) {
 document.getElementById('buttons').style.display = 'block';
 document.getElementById('content').innerHTML = '';
-var val = document.getElementById('search').value;
 var newelement = document.createElement('script');
-newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyDtMrQJ3KlkxY87DiLQFjkmgctz5niI9MU&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&callback=hndlr`;
+newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyDtMrQJ3KlkxY87DiLQFjkmgctz5niI9MU&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&cx=c02f25697ef11f644&callback=hndlr`;
 newelement.id = 'mainscript';
 document.body.appendChild(newelement);
 }
--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 function hndlr(response) {
-try {
-  document.getElementById('content').innerHTML += `
-  <div style="color: slategrey;">
-    Around ${response.searchInformation.formattedTotalResults} results in ${response.searchInformation.formattedSearchTime} seconds!
-  </div>`
-}
-catch(error) {
-document.getElementById('content').innerHTML = 'error!';
-}
 for (var i = 0; i < response.items.length; i++) { 
   document.getElementById('content').innerHTML += `
   <div style="align-items: center;">
-    <br>
     <a style="color: slategray; font-size: 12px; text-decoration: none;" href=${response.items[i].link} target="_blank">${response.items[i].link}</a>
     <a target="_blank" href=${response.items[i].link} style="text-decoration: none;">
       <h2 style="margin-top: 2px;">${response.items[i].title}</h2>
